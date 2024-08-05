4dcd9521546c89553ed3293038ef8161744f5617
kmh990303 <63326675+kmh990303@users.noreply.github.com>
2024-08-05 22:10:27 +0900

    Update Work.tsx

--- src/pages/Work.tsx
+++ src/pages/Work.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 interface Artwork {
     id: string;
@@ -15,6 +15,7 @@ interface formDataType {
 
 const Work: React.FC = () => {
     const location = useLocation();
+    const navigate = useNavigate();
     const art = location.state as Artwork;
     const [IsEdit, setIsEdit] = useState(false);
     const [formData, setFormData] = useState<formDataType>({
@@ -26,9 +27,7 @@ const Work: React.FC = () => {
         setIsEdit(true);
     }
 
-    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        e.preventDefault();
-
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
         const { name, value } = e.target;
 
         setFormData((prevState) => {
@@ -39,11 +38,32 @@ const Work: React.FC = () => {
         })
     }
 
-    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
 
+        try {
+            const response = await fetch(`http://localhost:5000/api/works/edit/${art.id}`, {
+                method: 'PATCH',
+                body: JSON.stringify(formData),
+                headers: {
+                    'Content-Type': 'application/json',
+                }
+            })
 
-    } //여기부터 다시 시작 -> work에 대한 api 구축 필요
+            if (!response.ok) {
+                throw new Error('Failed to update work');
+            }
+
+            const updatedWork = await response.json();
+            console.log(updatedWork);
+
+            setIsEdit(false);
+
+            navigate('/work');
+        } catch (error) {
+            console.error('Error updating work:', error);
+        }
+    }
 
     return (
         <>
@@ -66,13 +86,13 @@ const Work: React.FC = () => {
                 </div>
             )}
             {IsEdit && (
-                <div className='flex gap=8 mt-8 justify-center'>
+                <div className='flex gap-8 mt-8 justify-center'>
                     <div className='w-2/5 ml-8 translate-y-20'>
                         <img src={art.image} alt="art-image" />
                     </div>
                     <form method='PATCH' className='w-2/5 flex flex-col ml-8 translate-y-20' onSubmit={handleSubmit}>
-                        <input type="text" className='border-2 border-blue-500 rounded-xl mb-8 text-center p-8 text-4xl fond-bold' value={formData.title} onChange={handleChange} />
-                        <input type="text" className='border-2 border-blue-500 rounded-xl p-4 text-xl' value={formData.description} onChange={handleChange} />
+                        <input type="text" className='border-2 border-blue-500 rounded-xl mb-8 text-center p-8 text-4xl fond-bold' value={formData.title} onChange={handleChange} name='title' />
+                        <textarea className='border-2 border-blue-500 rounded-xl p-4 text-xl' value={formData.description} onChange={handleChange} name='description' />
                         <button className='border-2 border-blue-500 rounded-xl p-4 text-xl mt-36 hover:bg-blue-700' type='submit'>
                             Submit
                         </button>
