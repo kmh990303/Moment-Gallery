81534c8129d72d57704a28b812aa241657164dd0
kmh990303 <63326675+kmh990303@users.noreply.github.com>
2024-08-12 17:27:18 +0900

    Update Work.tsx

--- src/pages/Work.tsx
+++ src/pages/Work.tsx
@@ -1,5 +1,7 @@
-import React, { useState } from 'react';
-import { useLocation, useNavigate } from 'react-router-dom';
+import React, { useState, useContext } from 'react';
+import { useLocation, useNavigate, Link } from 'react-router-dom';
+import homeImg from '../assets/HomeMain.jpg';
+import { AuthContext } from '../shared/context/AuthContext';
 
 interface Artwork {
     id: string;
@@ -14,6 +16,7 @@ interface formDataType {
 }
 
 const Work: React.FC = () => {
+    const { isLoggedIn, logout } = useContext(AuthContext);
     const location = useLocation();
     const navigate = useNavigate();
     const art = location.state as Artwork;
@@ -94,45 +97,83 @@ const Work: React.FC = () => {
         }
     }
 
+    const handleLogout = () => {
+        alert('Are you really sure to logout?');
+        logout();
+    }
+
     return (
         <>
-            {!IsEdit && (
-                <div className='flex gap-8 mt-8 justify-center'>
-                    <div className='w-2/5 ml-8 translate-y-20'>
-                        <img src={`http://localhost:5000/${art.imagePath}`} alt="art-image" />
-                    </div>
-                    <div className='w-2/5 flex flex-col ml-8 translate-y-20'>
-                        <p className='border-2 border-blue-500 rounded-xl mb-8 text-center p-8 text-4xl fond-bold'>
-                            {art.title}
-                        </p>
-                        <p className='border-2 border-blue-500 rounded-xl p-4 text-xl'>
-                            {art.description}
-                        </p>
-                        <div className='flex gap-4'>
-                            <button className='w-1/2 border-2 border-blue-500 rounded-xl p-4 text-xl mt-36 hover:bg-blue-700' onClick={handleClickEdit}>
-                                Edit
-                            </button>
-                            <button className='w-1/2 border-2 border-blue-500 rounded-xl p-4 text-xl mt-36 hover:bg-red-700' onClick={handleDelete}>
-                                Delete
-                            </button>
+            {/* Header */}
+            <header className="fixed top-0 left-0 w-full flex justify-between items-center p-4 text-white z-20 bg-black bg-opacity-75">
+                <div className="ml-4 text-2xl font-bold cursor-pointer" onClick={() => navigate('/')}>
+                    Moment Gallery
+                </div>
+                <nav className="mr-8">
+                    <ul className="flex gap-8 text-xl">
+                        <li>
+                            <Link to='/'>Home</Link>
+                        </li>
+                        <li>
+                            <Link to='/work'>Work</Link>
+                        </li>
+                        {!isLoggedIn && (<li>
+                            <Link to='/login'>Login</Link>
+                        </li>)}
+                        {isLoggedIn && (
+                            <button onClick={handleLogout}>Logout</button>
+                        )}
+                        {isLoggedIn && (
+                            <Link to='/edit'>Create</Link>
+                        )}
+                    </ul>
+                </nav>
+            </header>
+            <main className='relative h-screen overflow-hidden bg-black bg-opacity-75'>
+                <div className='absolute inset-0'>
+                    <img src={homeImg} alt="Background" className='w-full h-full object-cover' />
+                </div>
+
+                <div className='relative z-10 flex flex-col items-center justify-start h-full'>
+                    {!IsEdit && (
+                        <div className='flex gap-8 mt-8 justify-center'>
+                            <div className='w-2/5 ml-8 translate-y-20'>
+                                <img src={`http://localhost:5000/${art.imagePath}`} alt="art-image" />
+                            </div>
+                            <div className='w-2/5 flex flex-col ml-8 translate-y-20'>
+                                <p className='rounded-xl mb-8 text-center p-8 text-4xl fond-bold text-white bg-black'>
+                                    {art.title}
+                                </p>
+                                <p className='rounded-xl p-4 text-xl text-center text-white bg-black'>
+                                    {art.description}
+                                </p>
+                                <div className='flex gap-4'>
+                                    <button className='w-1/2 rounded-xl p-4 text-xl mt-36 hover:bg-blue-700 text-white bg-black' onClick={handleClickEdit}>
+                                        Edit
+                                    </button>
+                                    <button className='w-1/2 rounded-xl p-4 text-xl mt-36 hover:bg-red-700 text-white bg-black' onClick={handleDelete}>
+                                        Delete
+                                    </button>
+                                </div>
+                            </div>
                         </div>
-                    </div>
+                    )}
+                    {IsEdit && (
+                        <div className='flex gap-8 mt-8 justify-center'>
+                            <div className='w-2/5 ml-8 translate-y-20'>
+                                <img src={`http://localhost:5000/${art.imagePath}`} alt="art-image" />
+                            </div>
+                            <form className='w-2/5 flex flex-col ml-8 translate-y-20' onSubmit={handleSubmit}>
+                                <input type="text" className='text-white bg-black rounded-xl mb-8 text-center p-8 text-2xl fond-bold' value={formData.title} onChange={handleChange} name='title' />
+                                <textarea className='text-white bg-black rounded-xl px-4 py-2 text-xl text-center' value={formData.description} onChange={handleChange} name='description' />
+                                <button className='text-white bg-black rounded-xl p-4 text-xl mt-36 hover:bg-blue-700  translate-y-2' type='submit'>
+                                    Submit
+                                </button>
+                            </form>
+                        </div>
+                    )}
                 </div>
-            )}
-            {IsEdit && (
-                <div className='flex gap-8 mt-8 justify-center'>
-                    <div className='w-2/5 ml-8 translate-y-20'>
-                        <img src={`http://localhost:5000/${art.imagePath}`} alt="art-image" />
-                    </div>
-                    <form className='w-2/5 flex flex-col ml-8 translate-y-20' onSubmit={handleSubmit}>
-                        <input type="text" className='border-2 border-blue-500 rounded-xl mb-8 text-center p-8 text-4xl fond-bold' value={formData.title} onChange={handleChange} name='title' />
-                        <textarea className='border-2 border-blue-500 rounded-xl p-4 text-xl' value={formData.description} onChange={handleChange} name='description' />
-                        <button className='border-2 border-blue-500 rounded-xl p-4 text-xl mt-36 hover:bg-blue-700' type='submit'>
-                            Submit
-                        </button>
-                    </form>
-                </div>
-            )}
+            </main>
         </>
     );
 };
