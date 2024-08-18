c1d38fc9cb6c9146c11c89e7137c689def771081
kmh990303 <63326675+kmh990303@users.noreply.github.com>
2024-08-18 15:24:01 +0900

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",  // 이 경로를 통해 Tailwind가 CSS 클래스를 추적할 수 있습니다.
+    "./src/**/*.{js,jsx,ts,tsx}", 
   ],
   theme: {
     extend: {
